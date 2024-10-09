d94dd0035c852a32966941306227d7be37b9af59
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-10-09 15:39:32 +0200

    created a grid

--- lib/components/table_card.dart
+++ lib/components/table_card.dart
@@ -1,16 +1,18 @@
 import "package:flutter/material.dart";
 
 class TableCard extends StatelessWidget {
-  const TableCard({super.key});
+   TableCard({super.key, required this.tablenumber});
+   var tablenumber;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Container(
-        color: Colors.brown,
+        color: Colors.yellow,
         height: 100,
         width: 100,
+        child: Text(tablenumber.toString()),
       ),
     );
   }
--- lib/table.dart
+++ lib/table.dart
@@ -3,25 +3,44 @@ import 'package:rest_auto/components/headline.dart';
 
 import 'components/table_card.dart';
 
-import 'components/table_card.dart';
 
-class TMS extends StatelessWidget {
+class TMS extends StatefulWidget {
   const TMS({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body:SafeArea(
-        child: Column( children:[
-          SizedBox(height: 20,),
-          Text("Restaurant",style: TextStyle(fontSize: 25),),
-          Center(child: Headline()),
-          SizedBox(height: 10,),
-          Text("Tables",style: TextStyle(fontSize: 20),),
-          SizedBox(height: 10,),
-          TableCard()
+  State<TMS> createState() => _TMSState();
+}
 
-        ]),
+class _TMSState extends State<TMS> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SingleChildScrollView(
+        child: SafeArea(
+          child: Column( children:[
+            const SizedBox(height: 20,),
+            const Text("Restaurant",style: TextStyle(fontSize: 25),),
+            const Center(child: Headline()),
+            const SizedBox(height: 10,),
+            const Text("Tables",style: TextStyle(fontSize: 20),),
+            const SizedBox(height: 10,),
+            SizedBox(
+              height: 500,
+              child: GridView.builder(
+                gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TableCard(tablenumber: index,),
+                  );
+                },
+                itemCount: 20,
+
+              ),
+            )]),
+        ),
       )
     );
   }
